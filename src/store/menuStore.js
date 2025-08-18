9830077f793826471123a4fd9a6f3609ac69901a
Huajieyu001 <331173500@qq.com>
2025-08-18 22:57:34 +0800

    axios向后端发请求

--- src/store/menuStore.js
+++ src/store/menuStore.js
@@ -3,17 +3,7 @@ import { defineStore } from "pinia";
 export const useMenuStore = defineStore('menuStore', {
     state: ()=>(
         {
-            menuList: [
-                {menuName: 'AAA', id: 0, isActive: true}, 
-                {menuName: 'BBB', id: 1, isActive: false}, 
-                {menuName: 'CCC', id: 2, isActive: false}, 
-                {menuName: 'DDD', id: 3, isActive: false}, 
-                {menuName: 'EEE', id: 4, isActive: false}, 
-                {menuName: 'FFF', id: 5, isActive: false}, 
-                {menuName: 'GGG', id: 6, isActive: false}, 
-                {menuName: 'HHH', id: 7, isActive: false}, 
-                {menuName: 'KKK', id: 8, isActive: false}
-            ]
+            menus: []
         }
     )
 })
